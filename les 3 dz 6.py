1f5f699ca81b9475ca54d5d909ca5ee50164305a
OlegLobanow <79658638+OlegLobanow@users.noreply.github.com>
2021-03-15 22:07:29 +0300

    Add files via upload

--- les 3 dz 6.py
+++ les 3 dz 6.py
@@ -2,5 +2,7 @@
 def int_func (*args):
     word = input("Введите слово с прописной буквы: ")
     print(word.title())
+    word_str = input("Введите слова прописными латинскими  буквами чере пробел: ")
+    print(word_str.title())
     return
 int_func()
